38cc4ad9cf35689bc8921a753493f48ff4d9078b
ryo-endo <ryo_endo@lockon.co.jp>
2017-02-20 11:24:26 +0900

    5

--- tests/Test.php
+++ tests/Test.php
@@ -2,7 +2,9 @@
 class FooTest extends PHPUnit_Framework_TestCase{
 
     public function testFoo(){
+    	echo "★";
+    	echo $PHP_ZTS;
+    	echo "★";
         $this->assertTrue(true);
-        
     }
 }
